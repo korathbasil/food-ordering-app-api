fe7d38b77538b7cf14ef252feccc73e3ad59f49d
Bazil Korath <korathbasil@live.com>
2021-07-11 23:06:32 +0530

    added node types

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,2 @@
-console.log("hello World");
+const express = require("express");
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,1 @@
-console.log("hello World");
+const express = require("express");
