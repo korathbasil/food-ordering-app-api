dfc7a2448b3a34b33879497c5b965c8fb0986038
Bazil Korath <korathbasil@live.com>
2021-07-14 19:52:16 +0530

    fix: Remove dist folder from git

--- dist/config/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_CONNECTION_URL = exports.PORT = void 0;
-exports.PORT = process.env.PORT || 8000;
-exports.DB_CONNECTION_URL = "mongodb://localhost:27017";
-//# sourceMappingURL=constants.js.map
