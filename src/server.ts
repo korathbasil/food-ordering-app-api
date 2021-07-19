adc23d5046b9ba2f836e71fb2d9d3df3605b1042
Bazil Korath <korathbasil@live.com>
2021-07-20 00:25:18 +0530

    add admin route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { PORT } from "./config/constants";
 import { connectToDatabase } from "./config/dbConnection";
 
+import adminRoute from "./routes/admin";
 import userRoute from "./routes/user";
 import restaurantRoute from "./routes/restaurant";
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 
 app.use("/api/restaurant", restaurantRoute);
+
+app.use("/admin", adminRoute);
 
 app.get("/", (_, res) => {
   res.send("hello World");
